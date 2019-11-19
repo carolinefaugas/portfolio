23e0a9d7f70f7f9454aed283cda67220441b7db7
caroline faugas <cfaugas@hotmail.com>
2019-11-19 19:15:29 +0100

    hopla

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def contact
   end
+
+  def download_pdf
+    send_file "#{Rails.root}/app/assets/docs/couteau_suisse.pdf", type: "application/pdf", x_sendfile: true
+  end
 end
